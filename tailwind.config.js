6d1deecfa9bcfb9e784d4eb6845c0dce23e98b91
Ananasik3455 <kirill2014657@yandex.ru>
2023-05-23 16:24:47 +0300

    Firsrt Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'sky-blue-paysum': '#61B2FC',
+        'green-paysum': '#11B849',
+
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
+
